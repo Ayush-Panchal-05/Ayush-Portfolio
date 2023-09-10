f157b24301c2a1356f5774cb999c732f96e2faec
Ayush Panchal <https://github.com/Ayush-panchal-05/News-App>
2023-09-10 14:28:01 +0530

    Portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var typed= new Typed(".text",{
+    strings: ["have solved 750 DSA questions on leetcode","am a Frontend Developer" ],
+    typeSpeed:70 ,
+    backSpeed: 70 ,
+    backDelay :1000 ,
+    loop: true,
+}) ;
